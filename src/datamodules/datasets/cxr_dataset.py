35ece6752267f28579dfae027d9ea430778660e3
Paul Wilson <pfrwilson@gmail.com>
2022-01-28 16:31:18 -0500

    major refactor

--- src/datamodules/datasets/cxr_dataset.py
+++ src/datamodules/datasets/cxr_dataset.py
@@ -1,3 +1,4 @@
+from curses import meta
 import torch
 from torch.utils.data import Dataset
 import SimpleITK as sitk
@@ -5,35 +6,60 @@ import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
 import random
+import numpy as np
 
 SEEDS = {
-    'train_test': 1, 
-    'train_val': 2
+    'test': 1, 
+    'val': 2
 }
 
 class CXRDataset(Dataset):
     
-    def __init__(self, root, split='train', resample_train_test=False):
+    def __init__(self, root, split='train', resample_val=False,
+                 ignore_negatives=False):
         
         self.root = root
         self.split = split
-        self.resample_train_test = resample_train_test
-        self.metadata = pd.read_csv(os.path.join(self.root, 'metadata.csv'), index_col=0)
-        self.idx2filepath = self.__get_indexing_splits(split)
+        
+        self.metadata = pd.read_csv(
+            os.path.join(root, 'metadata.csv'),
+            index_col=0
+        )
+
+        idx_df = self.__get_idx_df(root, resample_val)
+        
+        # add labels
+        idx_df = pd.merge(
+            idx_df, 
+            self.metadata[['img_name', 'label']].drop_duplicates(), 
+            on='img_name', 
+            how='inner'
+        )
+        
+        # extract split
+        if split:
+            assert split in ['train', 'val', 'test']
+            idx_df = idx_df.loc[idx_df['split'] == split]
+        
+        if ignore_negatives:
+            idx_df = idx_df.loc[idx_df['label'] == 1]
+        
+        self.idx_df = idx_df.reset_index(drop=True)
+        
         
     def __len__(self):
         return len(self.idx2filepath)
     
     def __getitem__(self, idx):
         
-        filepath = self.idx2filepath.loc[idx, 'filename']
-        item_metadata = self.metadata.loc[self.metadata.img_name == filepath]
+        img_name = self.idx_df.loc[idx, 'img_name']
+        item_metadata = self.metadata.loc[self.metadata.img_name == img_name]
         
         pixel_values = self.__read_mdh_to_numpy(
             os.path.join(
                 self.root, 
                 'images', 
-                filepath)
+                img_name)
         )
         
         label = item_metadata.loc[item_metadata.index[0], 'label']
@@ -51,35 +77,8 @@ class CXRDataset(Dataset):
             'bounding_boxes': bounding_boxes
         }
     
-        return pixel_values, label
+        return pixel_values, label      
     
-    def __get_indexing_splits(self, split):
-        
-        idx_df = pd.DataFrame(
-            os.listdir(os.path.join(self.root, 'images')), 
-            columns=['filename']
-        ).sort_values(by='filename').reset_index(drop=True)
-        
-        train_idx, test_idx = train_test_split(idx_df, train_size=0.8, 
-                                       random_state=SEEDS['train_test'])
-        
-        for df in (train_idx, test_idx):
-            df.reset_index(drop=True, inplace=True)
-        
-        train_idx, val_idx = train_test_split(train_idx, train_size=0.9,
-                                      random_state=SEEDS['train_val'] 
-                                      if self.resample_train_test else random.randint(10, 100))
-    
-        for df in (train_idx, val_idx):
-            df.reset_index(drop=True, inplace=True)
-            
-        if split == 'train':
-            return train_idx
-        elif split == 'val':
-            return val_idx
-        elif split == 'test':
-            return test_idx
-        else: raise ValueError('split must be one of ["train", "test", "val"].')        
     
     @staticmethod
     def __read_mdh_to_numpy(filename: str):
@@ -88,4 +87,36 @@ class CXRDataset(Dataset):
         array = sitk.GetArrayFromImage(img)
         
         return array        
+    
+    @staticmethod
+    def __get_idx_df(root, resample_val):
+        
+        idx_df = pd.DataFrame(
+                os.listdir(os.path.join(root, 'images')), 
+                columns=['img_name']
+            ).sort_values(by='img_name').reset_index(drop=True)
+        
+        idx_df['split'] = pd.Series(dtype='object')
+
+        # generate masks
+        
+        rng = np.random.RandomState(seed=SEEDS['test'])
+        test_mask = rng.rand(len(idx_df)) >= 0.8
+
+        rng = np.random.RandomState(seed=None if resample_val else SEEDS['val'])
+        val_mask = np.logical_and(rng.rand(len(idx_df)) >= 0.9, ~test_mask)
+
+        train_mask = np.logical_and(~val_mask, ~test_mask)
+        
+        idx_df['split'] = idx_df['split'].mask(test_mask, 'test')
+        idx_df['split'] = idx_df['split'].mask(val_mask, 'val')
+        idx_df['split'] = idx_df['split'].mask(train_mask, 'train')
+    
+        idx_df.to_csv(
+            os.path.join(root, 'split_info.csv'),
+            header=True
+        )
+        
+        return idx_df
+        
         
