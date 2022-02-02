da61ba82b419a94ecbd2f4ab0c6b4d96d50f0dd8
Paul Wilson <pfrwilson@gmail.com>
2022-02-01 21:47:51 -0500

    fixed bug in IoU calculator

--- src/utils/img_utils.py
+++ src/utils/img_utils.py
@@ -22,12 +22,14 @@ def iou(rect_1: Tuple[int, int, int, int],
     x_2, y_2, w_2, h_2 = rect_2 
     
     # length of width intersection 
-    x_m, x_M, w_m = (x_1, x_2, w_1) if x_1 <= x_2 else (x_2, x_1, w_2)
-    w_i = np.max( [x_m + w_m - x_M, 0] )
+    i_1 = max( x_1, x_2 )
+    i_2 = min( x_1 + w_1, x_2 + w_2 )
+    w_i = max( i_2 - i_1, 0 )
     
     # length of height intersection
-    y_m, y_M, w_m = (y_1, y_2, h_1) if y_1 <= y_2 else (y_2, y_1, h_2)
-    h_i = np.max( [y_m + w_m - y_M, 0] )
+    i_1 = max( y_1, y_2 )
+    i_2 = min( y_1 + h_1, y_2 + h_2 )
+    h_i = max( i_2 - i_1, 0 )
     
     # intersection area:
     i = w_i * h_i
@@ -36,3 +38,6 @@ def iou(rect_1: Tuple[int, int, int, int],
     u = ( w_1 * h_1 ) + ( w_2 * h_2 ) - i
     
     return i / u
+
+
+
