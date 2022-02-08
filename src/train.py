3252f08385c0d51d1c8640ef464a8abbd3bdd767
Paul Wilson <pfrwilson@gmail.com>
2022-02-08 10:34:46 -0500

    update configs

--- src/train.py
+++ src/train.py
@@ -11,14 +11,13 @@ from .models import RPNSystem
 
 def train(config: DictConfig): 
     
-    rpn = RPNSystem(**config.rpn)
+    rpn = RPNSystem(**config.model.rpn)
     
     datamodule = CXRDataModule(
         **config.data
     )
     
     trainer = Trainer(
-        gpus=1 if torch.cuda.is_available() else 0
         **config.trainer
     )
     
