aa1f4bb7d9ee08c17fa218de7ecc88c209e7e477
Paul Wilson <pfrwilson@gmail.com>
2022-02-14 16:37:48 -0500

    added wandb logging

--- src/train.py
+++ src/train.py
@@ -1,7 +1,7 @@
 from re import T
 from omegaconf import DictConfig
 from pytorch_lightning import Trainer
-
+from pytorch_lightning.loggers import WandbLogger
 
 from .data import CXRDataModule
 import torch
@@ -12,9 +12,7 @@ from .models.metric import DetectionMetric
 
 def train(config: DictConfig): 
     
-    metric = DetectionMetric(
-        **config.metrics
-    )
+    metric = DetectionMetric()
     
     rpn = RPNSystem(metric, **config.model.rpn)
     
@@ -22,8 +20,11 @@ def train(config: DictConfig):
         **config.data
     )
     
+    logger = WandbLogger(project='lungbot')
+    
     trainer = Trainer(
-        **config.trainer
+        **config.trainer,
+        logger=logger
     )
     
     trainer.fit(rpn, datamodule)
