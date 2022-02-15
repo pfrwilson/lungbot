9de1d073a709aacea4a41120b9793928f6edfbe1
Paul Wilson <pfrwilson@gmail.com>
2022-02-15 11:20:00 -0500

    added disable warnings option

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import hydra
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 
 @hydra.main(config_path='configs', config_name='config')
 def main(config):
@@ -7,12 +7,16 @@ def main(config):
     from src.train import train
     from src import utils
     
-    from typing import List, Sequence
-
     # Pretty print config using Rich library
     if config.get("print_config"):
         utils.print_config(config, resolve=True)
-        
+
+    # disable warnings
+    if config.get("disable_warnings"):
+        import warnings
+        print("Disabling warnings.")
+        warnings.filterwarnings('ignore')
+    
     return train(config)
 
 
