7a12894e198aec1b35d56c265b806e33b1a531a6
Paul Wilson <pfrwilson@gmail.com>
2022-02-15 16:28:48 -0500

    linting

--- main.py
+++ main.py
@@ -2,16 +2,14 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 
 @hydra.main(config_path='configs', config_name='config')
-def main(config):
+def main(config: DictConfig):
     
     from src.train import train
     from src import utils
-    
-    # Pretty print config using Rich library
+
     if config.get("print_config"):
         utils.print_config(config, resolve=True)
 
-    # disable warnings
     if config.get("disable_warnings"):
         import warnings
         print("Disabling warnings.")
