a13a21f39928891bbcb12193a9583210e69f0379
Paul Wilson <pfrwilson@gmail.com>
2022-02-15 16:29:29 -0500

    added callbacks

--- src/train.py
+++ src/train.py
@@ -1,28 +1,36 @@
-from re import T
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import WandbLogger
-
-from .data import CXRDataModule
-import torch
+from pytorch_lightning.callbacks import EarlyStopping
 
 from .data import CXRDataModule
 from .models.rpn_module import RPNModule, RPNModuleConfig
-from .models.metric import DetectionMetric
+
+import wandb
+
 
 def train(config: DictConfig): 
-    
-    rpn = RPNModule(RPNModuleConfig(**config.model.rpn))
+     
+    rpn = RPNModule(
+        RPNModuleConfig(**config.model.rpn)
+    )
     
     datamodule = CXRDataModule(
         **config.data
     )
     
-    logger = WandbLogger(project='lungbot')
+    logger = WandbLogger(
+        **config.logger
+    )
+    
+    callbacks = [
+        EarlyStopping(monitor='val/precision', patience=10)
+    ]
     
     trainer = Trainer(
         **config.trainer,
-        logger=logger
+        logger=logger, 
+        callbacks=callbacks
     )
     
     trainer.fit(rpn, datamodule)
