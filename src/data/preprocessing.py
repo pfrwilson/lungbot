0ffc5eddfe59f50df16ec948aedc41542af13fb5
ayeshans <ayeshanaeem604@gmail.com>
2022-02-18 13:51:46 -0500

    added sobel filter

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -1,7 +1,16 @@
 import torchvision
+from torch import Tensor
+from torch.nn import Conv2d
 
 # TODO 
 
-def preprocessor_factory(equalize_hist):
+def preprocessor_factory(equalize_hist, preprocessing_sharpen):
+    transforms = []
     if equalize_hist:
-        return torchvision.transforms.functional.equalize
+        transforms += [torchvision.transforms.functional.equalize]
+    if preprocessing_sharpen:
+        filter = Tensor([[1, 0, -1], 
+                                [2, 0, -2], 
+                                [1, 0, -1]])
+        transforms += [Conv2d(filter)]
+    return transforms
